a7ae312069bc46c1ead18fbd1032ddc2e59dc6b9
parkh <parkh.dan@gmail.com>
2022-10-29 22:37:43 +0000

    Refactor

--- main.py
+++ main.py
@@ -4,62 +4,61 @@ from datetime import datetime
 alphabet = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
 
 
-def coder(message, key, encode):
+def coder(text, key, encode):
     result = ''
     index = 0
 
-    for symbol in message:
+    for symbol in text:
         if symbol not in alphabet:
             result += symbol
             continue
 
-        current_letter_index = alphabet.index(symbol)
-
+        index_in_alphabet = alphabet.index(symbol)
         index_in_key = index % len(key)
-        number_string = key[index_in_key]
-        number = int(number_string)
+        digit_from_key = key[index_in_key]
+        digit_from_key = int(digit_from_key)
 
         if encode:
-            new_letter_index = current_letter_index + number
+            new_index = index_in_alphabet + digit_from_key
         else:
-            new_letter_index = current_letter_index - number
-
-        new_letter_index = new_letter_index % len(alphabet)
-        new_letter = alphabet[new_letter_index]
+            new_index = index_in_alphabet - digit_from_key
+        new_index = new_index % len(alphabet)
+        new_symbol = alphabet[new_index]
 
         index += 1
-        result += new_letter
+        result += new_symbol
 
     return result
 
 
 def main():
-    the_type = input('Введіть `1` якщо Ви хочете зашифрувати, і `0` якщо ви хочете розшифрувати:\n')
-
-    if the_type == '1':
+    option = input('Введіть `1` якщо Ви хочете зашифрувати, і `0` якщо ви хочете розшифрувати: ')
+    print('\n')
+    if option == '1':
         encode = True
-    elif the_type == '0':
+    elif option == '0':
         encode = False
     else:
         main()
         return
 
-    date = input('Введіть дату (dd/mm/yyyy):\n')
 
+    date = input('Введіть дату (dd/mm/yyyy): ')
+    print('\n')
     try:
         date_object = datetime.strptime(date, '%d/%m/%Y')
     except:
         main()
         return
-
     key = date_object.strftime('%d%m%Y')
 
-    message = input('Введіть текст:\n')
-    message = message.lower()
 
-    result = coder(message, key, encode)
+    text = input('Введіть текст: ')
+    print('\n')
+    text = text.lower()
 
-    print(f'Результат: {result}')
+    result = coder(text, key, encode)
+    print(f'Результат: {result}\n')
 
 
 main()
